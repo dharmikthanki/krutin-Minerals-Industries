ca2037293313f366d8145045da5ca70efe6662b0
Dharmik Thanki <88383036+dharmikthanki@users.noreply.github.com>
2024-07-09 09:11:02 +0530

    Delete krutin2 directory

--- krutin2/admin/delete2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-require('db.php');
-$id=$_REQUEST['id'];
-$query = "DELETE FROM book_form WHERE id=$id"; 
-$result = mysqli_query($con,$query) or die ( mysqli_error());
-header("Location: app.php"); 
-?> 
